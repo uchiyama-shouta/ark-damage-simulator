2831a560c0f38b957acb63d891a06a859765bd79
utiyama <ninjin0604@gmail.com>
2022-05-25 02:59:57 +0900

    UIの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,30 +16,31 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <div>
-        <label htmlFor="name">恐竜の名前</label>
-        <Autocomplete
-          id="name"
-          onChange={handleName}
-          value={name}
-          data={data.map((data) => data.dinosaur)}
-          classNames={{
-            input: "text-xl",
-          }}
-        />
+        <label>
+          恐竜の名前
+          <Autocomplete
+            // id="name"
+            onChange={handleName}
+            value={name}
+            data={data.map((data) => data.dinosaur)}
+            classNames={{
+              input: "text-xl",
+            }}
+          />
+        </label>
 
         <div className="h-5" />
-        <label htmlFor="melee" className="block font-medium">
+        <label className="block font-medium">
           攻撃力(%の入力は不要)
+          <div className="mt-1">
+            <input
+              type="number"
+              className="block py-1 w-full text-xl font-medium rounded-md border-gray-300 focus:border-[#339af0] focus:ring-[#339af0]"
+              value={melee || ""}
+              onChange={handleMelee}
+            />
+          </div>
         </label>
-        <div className="mt-1">
-          <input
-            type="number"
-            id="melee"
-            className="block py-1 w-full text-xl font-medium rounded-md border-gray-300 focus:border-[#339af0] focus:ring-[#339af0]"
-            value={melee || ""}
-            onChange={handleMelee}
-          />
-        </div>
       </div>
       <p className="mt-7 text-center">
         トータルダメージ＝
